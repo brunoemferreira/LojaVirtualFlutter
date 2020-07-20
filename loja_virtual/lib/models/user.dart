4553738da36b954dc2f9fe3c25f2063cbac1cc68
bemferreira <brunoemf@gmail.com>
2020-07-20 20:45:31 -0300

    Código da Aula 26

--- loja_virtual/lib/models/user.dart
+++ loja_virtual/lib/models/user.dart
@@ -1,11 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User {
   // Construtor
-  User({this.email, this.password, this.name});
+  User({this.email, this.password, this.name, this.id});
 
   // Atributos da classe
+  String id;
   String name;
   String email;
   String password;
 
   String confirmPassword;
+
+  DocumentReference get firestoreRef =>
+      Firestore.instance.document('users/$id');
+
+  // Salva os dados do usuário
+  Future<void> saveData() async {
+    await firestoreRef.setData(toMap());
+  }
+
+  Map<String, dynamic> toMap(){
+    return {
+      'name': name,
+      'email': email,
+    };
+  }
 }
