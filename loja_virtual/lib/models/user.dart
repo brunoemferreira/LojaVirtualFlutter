4ce0e9f56fc5adcf56555f868cf43e5c506cd2cd
bemferreira <brunoemf@gmail.com>
2020-07-20 20:26:18 -0300

    Código Aula 25

--- loja_virtual/lib/models/user.dart
+++ loja_virtual/lib/models/user.dart
@@ -1,8 +1,11 @@
 class User {
   // Construtor
-  User({this.email, this.password});
+  User({this.email, this.password, this.name});
 
   // Atributos da classe
+  String name;
   String email;
   String password;
+
+  String confirmPassword;
 }
--- loja_virtual/lib/models/user_manager.dart
+++ loja_virtual/lib/models/user_manager.dart
@@ -34,11 +34,28 @@ class UserManager extends ChangeNotifier {
     loading = false;
   }
 
+  // Cadastro do Usuário
+  Future<void> signUp({User user, Function onFail, Function onSuccess}) async {
+    loading = true;
+    try {
+      final AuthResult result = await auth.createUserWithEmailAndPassword(
+          email: user.email, password: user.password);
+
+      this.user = result.user;
+
+      onSuccess();
+    } on PlatformException catch (e){
+      onFail(getErrorString(e.code));
+    }
+    loading = false;
+  }
+
   set loading(bool value) {
     _loading = value;
     notifyListeners();
   }
 
+  // Carrega o usuário Corrente
   Future<void> _loadCurrentUser() async {
     final FirebaseUser currentUser = await auth.currentUser();
     if (currentUser != null) {
